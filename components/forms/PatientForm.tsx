b4880bfd0334e6c2661890e416010903e6812327
Achouak Cherif <a.cherif@esi-sba.dz>
2024-10-23 00:04:19 +0200

    Finishing home page and setting appwrite

--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -9,10 +9,14 @@ import {
     Form
 } from "@/components/ui/form"
 import CustomFormField from "../CustomFormField"
+import SubmitButton from "../SubmitButton"
+import { useState } from "react"
+import { UserFormValidation } from "@/app/lib/validation"
+import { useRouter } from "next/navigation"
 
 export enum FormFieldType {
     INPUT = 'input',
-    TEXTAREA= 'textarea',
+    TEXTAREA = 'textarea',
     PHONE_INPUT = 'phoneInput',
     CHECKBOX = 'checkox',
     DATE_PICKER = 'datePicker',
@@ -20,43 +24,80 @@ export enum FormFieldType {
     SKELETON = 'skeleton',
 }
 
-const formSchema = z.object({
-    username: z.string().min(2, {
-        message: "Username must be at least 2 characters.",
-    }),
-})
+
 
 const PatientForm = () => {
+    const router = useRouter();
+    const [isLoading, setIsLoading]= useState(false);
 
-    const form = useForm<z.infer<typeof formSchema>>({
-        resolver: zodResolver(formSchema),
+    const form = useForm<z.infer<typeof UserFormValidation>>({
+        resolver: zodResolver(UserFormValidation),
         defaultValues: {
-            username: "",
+            name: "",
+            email:"",
+            phone:"",
         },
     })
 
 
-    function onSubmit(values: z.infer<typeof formSchema>) {
-        // Do something with the form values.
-        // ✅ This will be type-safe and validated.
-        console.log(values)
-    }
+    const onSubmit = async (values: z.infer<typeof UserFormValidation>) => {
+        setIsLoading(true);
+    
+        try {
+         /* const user = {
+            name: values.name,
+            email: values.email,
+            phone: values.phone,
+          };
+    
+          const newUser = await createUser(user);
+    
+          if (newUser) {
+            router.push(`/patients/${newUser.$id}/register`);
+          }*/
+        } catch (error) {
+          console.log(error);
+        }
+    
+        setIsLoading(false);
+      };
+    
     return (
         <Form {...form}>
             <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6 flex-1">
-                <section className="m-12 space-y-4">
+                <section className="mb-12 space-y-4">
                     <h1 className="header">Hi there 👋</h1>
                     <p className="text-dark-700">Schedule your first appointment.</p>
                 </section>
+
                 <CustomFormField
-                fieldType={FormFieldType.INPUT}
-                 control={form.control}
-                 name="name"
-                 label="Full name"
-                 placeholder="John Doe"
-                 iconSrc="/assets/icons/user.svg"
-                 iconAlt="user" />
-                <Button type="submit">Submit</Button>
+                    fieldType={FormFieldType.INPUT}
+                    control={form.control}
+                    name="name"
+                    label="Full name"
+                    placeholder="John Doe"
+                    iconSrc="/assets/icons/user.svg"
+                    iconAlt="user" />
+
+                <CustomFormField
+                    fieldType={FormFieldType.INPUT}
+                    control={form.control}
+                    name="email"
+                    label="Email"
+                    placeholder="johndoe@gmail.com"
+                    iconSrc="/assets/icons/email.svg"
+                    iconAlt="email" />
+
+                <CustomFormField
+                    fieldType={FormFieldType.PHONE_INPUT}
+                    control={form.control}
+                    name="phone"
+                    label="Phone number"
+                    placeholder="(213) 123-4567"
+                   />
+                <SubmitButton isLoading={isLoading}>
+                    Get Started
+                </SubmitButton>
             </form>
         </Form>
     )
